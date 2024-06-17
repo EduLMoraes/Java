043c64af3cd50495a20e23defe3b4796795f3817
EduLMoraes <17eduardo05@gmail.com>
2024-06-17 15:24:51 -0300

    modified:   BiTree/BiTree.java 	new file:   entrevista/Carro.class 	new file:   entrevista/Carro.java 	new file:   entrevista/Fors.class 	new file:   entrevista/Fors.java 	new file:   entrevista/texto.txt

---
+++ entrevista/Carro.java
@@ -0,0 +1,19 @@
+public class Carro{
+    public static void main(String args[]){
+        // Carro carro = new Carro();
+        // carro.anda();
+        new Carro().anda();
+
+        System.out.println("String"+3+7+"String");
+    }
+
+
+    public static void anda(){
+        System.out.println("andando...");
+        para();
+    }
+
+    public static void para(){
+        System.out.println("parando...");
+    }
+}
